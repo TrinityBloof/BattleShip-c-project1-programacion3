bade4b049dd8ee0ef8ef05fc27b8b6cc0ace25c9
TrinityBloof <97478521+TrinityBloof@users.noreply.github.com>
2022-06-01 22:23:13 -0600

    Add files via upload

---
+++ battleship/Mapa.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace battleship
+{
+    internal class Mapa
+    {
+
+        public static void imprimirMapa(int[,] mapaPlayer, int[,] mapaShots)
+        {
+            char letra = 'A';
+            Console.WriteLine("--------------Mapa del jugador--------------");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("[ ] [0] [1] [2] [3] [4] [5] [6] [7] [8] [9]");
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i < mapaPlayer.GetLength(0); i++)
+            {
+                for (int j = 0; j < mapaPlayer.GetLength(1); j++)
+                {
+                    if (mapaPlayer[i, j] == 0)
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write("[" + letra + "] ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            letra++;
+                        }
+                        if (j == 9 && i < 9)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.Write("[*] ");
+                            Console.WriteLine("");
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write("[" + letra + "] ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            letra++;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.Write("[*] ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    } else if (mapaPlayer[i, j] == 1)
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write("[" + letra + "] ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            letra++;
+                        }
+                        if (j == 9 && i < 9)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("[*] ");
+                            Console.WriteLine("");
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write("[" + letra + "] ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            letra++;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("[*] ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    }
+                    else
+                    {
+                        if (j == 9)
+                        {
+                            Console.WriteLine("");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(mapaPlayer[i, j] + " ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine("");
+            letra = 'A';
+            SetCursorPosition(50, 0);
+            Console.WriteLine("---------------Mapa de tiros---------------");
+            SetCursorPosition(50, 1);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("[ ] [0] [1] [2] [3] [4] [5] [6] [7] [8] [9]");
+            Console.ForegroundColor = ConsoleColor.White;
+            SetCursorPosition(50, 2);
+            for (int i = 0; i < mapaShots.GetLength(0); i++)
+            {
+                if (letra != 'A')
+                SetCursorPosition(54, i+2);
+                for (int j = 0; j < mapaShots.GetLength(1); j++)
+                {
+                    if (mapaShots[i, j] == 0)
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write("[" + letra + "] ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            letra++;
+                        }
+                        if (j == 9 && i < 9)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.Write("[*] ");
+                            Console.WriteLine("");
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            SetCursorPosition(50, i+3);
+                            Console.Write("[" + letra + "] ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            letra++;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.Write("[*] ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    }
+                    else
+                    {
+                        if (j == 9)
+                        {
+                            Console.WriteLine("");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(mapaShots[i, j] + " ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    }
+
+                }
+
+            }
+        }
+    }
+}
---
+++ battleship/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace battleship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //matriz mapa jugador1
+            int[,] mapaPlayer = new int[10, 10];
+            //matriz mapa tiros realizados
+            int[,] mapaShots = new int[10, 10];
+            //matriz mapa jugador 2
+            int[,] mapaPlayer2 = new int[10, 10];
+            //matriz mapa tiros realizados
+            int[,] mapaShots2 = new int[10, 10];
+            //pos x y
+            int x = 5, y = 2, x2 = 5, y2 = 2;
+            //turno y barcos
+            int turno = 1, barco = 5, barco2 = 5;
+
+        juego:
+            if (barco == 0 && barco2 == 0)
+            {
+                SetCursorPosition(0, 0);
+                if (turno == 1)
+                {
+                    x = 55;
+                    y = 2;
+                    Mapa.imprimirMapa(mapaPlayer, mapaShots);
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("*************************************Turno del jugador " + turno + "*************************************");
+                    Console.WriteLine("              Usa WASD para moverte y disparar en el terreno");
+                    Console.WriteLine("                 (Solo puedes interactuar con los * del terreno)");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    SetCursorPosition(x, y);
+                }
+                else if (turno == 2)
+                {
+                    x2 = 55;
+                    y2 = 2;
+                    Mapa.imprimirMapa(mapaPlayer2, mapaShots2);
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("*************************************Turno del jugador " + turno + "*************************************");
+                    Console.WriteLine("                  Usa WASD para moverte y disparar en el terreno");
+                    Console.WriteLine("                 (Solo puedes interactuar con los * del terreno)");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    SetCursorPosition(x2, y2);
+                }
+            }
+            else
+            {
+                SetCursorPosition(0, 0);
+                if (turno == 1)
+                {
+                    Mapa.imprimirMapa(mapaPlayer, mapaShots);
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("*************************************Turno del jugador " + turno + "*************************************");
+                    Console.WriteLine("             Usa WASD para moverte por el terreno y colocar tus barcos");
+                    Console.WriteLine("                 (Solo puedes interactuar con los * del terreno)");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    SetCursorPosition(x, y);
+                }
+                else if (turno == 2)
+                {
+                    Mapa.imprimirMapa(mapaPlayer2, mapaShots2);
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("*************************************Turno del jugador " + turno + "*************************************");
+                    Console.WriteLine("             Usa WASD para moverte por el terreno y colocar tus barcos");
+                    Console.WriteLine("                 (Solo puedes interactuar con los * del terreno)");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    SetCursorPosition(x2, y2);
+                }
+
+                ConsoleKeyInfo keyInfo;
+
+                while ((keyInfo = Console.ReadKey(true)).Key != ConsoleKey.Escape)
+                {
+                    switch (keyInfo.Key)
+                    {
+                        case ConsoleKey.W:
+                            if (turno == 1)
+                            {
+                                if (y > 2)
+                                {
+                                    SetCursorPosition(x, y - 1);
+                                    y--;
+                                }
+                            }
+                            if (turno == 2)
+                            {
+                                if (y2 > 2)
+                                {
+                                    SetCursorPosition(x2, y2 - 1);
+                                    y2--;
+                                }
+                            }
+                            break;
+
+                        case ConsoleKey.D:
+                            if (turno == 1)
+                            {
+                                if (x < 41)
+                                {
+                                    SetCursorPosition(x + 1, y);
+                                    x++;
+                                }
+                            }
+                            if (turno == 2)
+                            {
+                                if (x2 < 41)
+                                {
+                                    SetCursorPosition(x2 + 1, y2);
+                                    x2++;
+                                }
+                            }
+                            break;
+
+                        case ConsoleKey.S:
+                            if (turno == 1)
+                            {
+                                if (y < 11)
+                                {
+                                    SetCursorPosition(x, y + 1);
+                                    y++;
+                                }
+                            }
+                            if (turno == 2)
+                            {
+                                if (y2 < 11)
+                                {
+                                    SetCursorPosition(x2, y2 + 1);
+                                    y2++;
+                                }
+                            }
+                            break;
+
+                        case ConsoleKey.A:
+                            if (turno == 1)
+                            {
+                                if (x > 5)
+                                {
+                                    SetCursorPosition(x - 1, y);
+                                    x--;
+                                }
+                            }
+                            if (turno == 2)
+                            {
+                                if (x2 > 5)
+                                {
+                                    SetCursorPosition(x2 - 1, y2);
+                                    x2--;
+                                }
+                            }
+                            break;
+                        case ConsoleKey.Spacebar:
+                            if (turno == 1)
+                            {
+                                if (x == 5)
+                                {
+                                    int ayuda = y - 2;
+                                    if (mapaPlayer[0+ayuda,y - 2 - ayuda] == 0)
+                                    {
+                                        mapaPlayer[0+ayuda, y-2-ayuda] = 1;
+                                        barco--;
+                                        if (barco == 0)
+                                        {
+                                            turno = 2;
+                                            goto juego;
+                                        } else
+                                        {
+                                            goto juego;
+                                        }
+                                    }
+                                }
+                            }
+                            if (turno == 2)
+                            {
+                                if (x2 == 5)
+                                {
+                                    int ayuda = y2 - 2;
+                                    if (mapaPlayer2[0 + ayuda, y2 - 2 - ayuda] == 0)
+                                    {
+                                        mapaPlayer2[0 + ayuda, y2 - 2 - ayuda] = 1;
+                                        barco2--;
+                                        if (barco2 == 0)
+                                        {
+                                            turno = 1;
+                                            goto juego;
+                                        }
+                                        else
+                                        {
+                                            goto juego;
+                                        }
+                                    }
+                                }
+                            }
+                            break;
+                    }
+                }
+            }
+
+        }
+    }
+}
